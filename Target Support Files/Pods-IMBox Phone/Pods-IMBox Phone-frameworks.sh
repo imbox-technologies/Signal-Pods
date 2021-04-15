3fec3abc6592948093fa15b1e833432666bc0720
Álvaro Marcos <alvaro.marcos@spotbros.com>
2021-04-15 11:43:33 +0200

    Fix script (fails to Archive)

--- Target Support Files/Pods-IMBox Phone/Pods-IMBox Phone-frameworks.sh
+++ Target Support Files/Pods-IMBox Phone/Pods-IMBox Phone-frameworks.sh
@@ -1,19 +1,4 @@
 #!/bin/sh
-                
-# ---- this is added by cocoapods-binary ---
-# Readlink cannot handle relative symlink well, so we override it to a new one
-# If the path isn't an absolute path, we add a realtive prefix.
-old_read_link=`which readlink`
-readlink () {
-    path=`$old_read_link $1`;
-    if [ $(echo "$path" | cut -c 1-1) = '/' ]; then
-        echo $path;
-    else
-        echo "`dirname $1`/$path";
-    fi
-}
-# --- 
-#!/bin/sh
 set -e
 set -u
 set -o pipefail
